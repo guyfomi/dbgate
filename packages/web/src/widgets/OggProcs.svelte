a7e6f81068743f822826f3078627606bc239b15f
Guy Fomi <guyfomi@gmail.com>
2024-06-06 19:00:54 +0100

    updated oggproc

--- packages/web/src/widgets/OggProcs.svelte
+++ packages/web/src/widgets/OggProcs.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
 
-  const dispatch = createEventDispatcher();  
+  const dispatch = createEventDispatcher();
+
+  function handleClick(x) {
+    console.log('button clicked ...');
+    console.debug(x);
+  }
 
   let promise = fetchData();
 
   async function fetchData() {
     const res = await fetch('http://10.100.18.17/bi_server/admin/json.php?module=goldengate&action=proc_states');
-    const data = await res.json();    
+    const data = await res.json();
 
     if (res.ok) {
       return data;
@@ -24,7 +29,11 @@
     <p>loading</p>
   {:then items}
     {#each items as item}
-      <tr><td>{item.process}</td><td>{item.state}</td><td>{item.lag}</td><td><button type="button">{item.action}</button></td></tr>
+      <tr
+        ><td>{item.process}</td><td>{item.state}</td><td>{item.lag}</td><td
+          ><button on:click={handleClick} type="button">{item.action}</button></td
+        ></tr
+      >
     {/each}
   {:catch error}
     <p style="color: red">{error.message}</p>
